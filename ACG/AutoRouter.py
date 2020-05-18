972d082b370490c461b45ea6698db29f08cbee55
John Fini <johnmfini@gmail.com>
2020-05-18 16:40:43 -0700

    error commented out for debugging

--- ACG/AutoRouter.py
+++ ACG/AutoRouter.py
@@ -680,19 +680,29 @@ class EZRouter:
             #          final_point_list[index_point + 1]))
             current_dir_dim = 0 if ('x' in self.current_dir) else 1
             current_dir_sgn = 1 if ('+' in self.current_dir) else -1
-            current_pos_dim = (self.current_rect.loc['c'].x if ('x' in self.current_dir) 
-                               else self.current_rect.loc['c'].y)
+            if ('+x' == self.current_dir):
+                current_pos_dim = self.current_rect.loc['l']
+            elif('-x' == self.current_dir):
+                current_pos_dim = self.current_rect.loc['r']
+            elif('+y' == self.current_dir):
+                current_pos_dim = self.current_rect.loc['b']
+            elif('-y' == self.current_dir):
+                current_pos_dim = self.current_rect.loc['t']
+            # current_pos_dim = (self.current_rect.loc['c'].x if ('x' in self.current_dir) 
+            #                    else self.current_rect.loc['c'].y)
             forward = current_dir_sgn * (final_point_list[index_point][0][current_dir_dim] - current_pos_dim)
             if( forward < -1e-3): 
                 print('In AutoRouter, testing that next point is towards the current_dir:')
                 print(final_point_list[index_point + 1])
+                print(self.current_dir)
                 print(self.current_rect)
+                print(self.current_rect.loc)
                 print(current_dir_sgn)
                 print(current_pos_dim)
                 print('From %f to %f' % (current_pos_dim, final_point_list[index_point][0][current_dir_dim]))
                 print('This should not be negative: ')
                 print(forward)
-                raise RuntimeError('routing point is not in current_dir')
+                # raise RuntimeError('routing point is not in current_dir')
 
             self._draw_route_segment(pt0=final_point_list[index_point],
                                      pt1=final_point_list[index_point + 1],
